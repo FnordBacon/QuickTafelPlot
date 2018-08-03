55a3995f84c56d20855f39588d668496e72a1302
FnordBacon <jbb1415@gmail.com>
2018-08-03 00:45:23 -0700

    its alive!

--- Main.py
+++ Main.py
@@ -13,9 +13,10 @@ def GetFilepathArray(rootDir):
         for file in files:
             # only gets files with the mpt extension
             if 'mpt' in file:
-                dirpaths.append(subdir)
-                fileNames.append(file)
-                filepaths.append(os.path.join(subdir, file))
+                if 'OCV' not in file:
+                    dirpaths.append(subdir)
+                    fileNames.append(file)
+                    filepaths.append(os.path.join(subdir, file))
 
     return dirpaths, fileNames, filepaths
 
@@ -44,7 +45,7 @@ def find_start_index(path):
 def data_extractor(file_path):
     raw_data = pd.read_csv(file_path, sep='\t', skiprows=find_start_index(file_path))
     potential = [value for value in raw_data["Ewe/V"]]
-    current = [value for value in raw_data["<I>/mA"]]
+    current = [value*1000 for value in raw_data["<I>/mA"]]
     return potential, current
 
 
@@ -61,7 +62,7 @@ def PlotAndSaveLPRGraph(filepathToSaveTo, ArrayToPlot, xAxisLabel, yAxisLabel):
     # sets the plot to about the size of a piece of paper
     plotter.figure(figsize=(11, 8.5), dpi=600)
     # in puts the data to the plotter
-    plotter.plot(ArrayToPlot[0], ArrayToPlot[1], ".", markersize=3)
+    plotter.plot(ArrayToPlot[0], ArrayToPlot[1], ".", markersize=1)
     plotter.ylabel(yAxisLabel)
     plotter.xlabel(xAxisLabel)
     plotter.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
@@ -71,16 +72,26 @@ def PlotAndSaveLPRGraph(filepathToSaveTo, ArrayToPlot, xAxisLabel, yAxisLabel):
 
 def PlotAndSaveTPGraph(filepathToSaveTo, ArrayToPlot, xAxisLabel, yAxisLabel):
     plotter.figure(figsize=(11, 8), dpi=600)
-    plotter.semilogx(GetAbsOfArray(ArrayToPlot[0]), ArrayToPlot[1], ".", markersize=3)
+    plotter.semilogx(GetAbsOfArray(ArrayToPlot[0]), ArrayToPlot[1], ".", markersize=1)
     plotter.ylabel(yAxisLabel)
     plotter.xlabel(xAxisLabel)
     plotter.savefig(filepathToSaveTo, bbox_inches='tight')
     # plotter.show()
 
 
-rootDir = r'M:\myWorkShop\pyWorkShop\Data to process\Test 018'
+rootDir = r'M:\myWorkShop\pyWorkShop\Data to process'
+saveDir = r"M:\myWorkShop\pyWorkShop\Graphs"
 dirPath, fileName, filePaths = GetFilepathArray(rootDir)
 
 for entry in filePaths:
 
-    data = data_extractor(entry)
+    Voltage, Current = data_extractor(entry)
+
+    GraphFileName = entry.split("\\")[-2]
+    print(GraphFileName)
+
+    if "LPR" in entry:
+        PlotAndSaveLPRGraph(saveDir + "\\LPR_" + str(GraphFileName) + ".pdf", [Current, Voltage], 'Current (mA)', 'Voltage (V vs SCE)')
+
+    if "TP" in entry:
+        PlotAndSaveTPGraph(saveDir + "\\TP_" + str(GraphFileName) + ".pdf", [Current, Voltage], 'Current (mA)', 'Voltage (V vs SCE)')
